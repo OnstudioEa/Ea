41d5b326081606f9e8384900daf553cdf287a84f
DESKTOP-778U4BD\김광준 <ktf0723@naver.com>
2017-01-19 02:48:00 +0900

    영상삽입 완료 유아이 해상도에 따른 스케일조정 완료

--- Assets/ScriptsFolder/MovieManager.cs
+++ Assets/ScriptsFolder/MovieManager.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(AudioSource))]
 public class MovieManager : MonoBehaviour {
     
     // Use this for initialization
     void Awake()
     {
-        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+        Handheld.PlayFullScreenMovie("title.mp4", Color.black, FullScreenMovieControlMode.Hidden);
     }
 }
---
+++ Assets/ScriptsFolder/StartSceneManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartSceneManager : MonoBehaviour {
+
+    public UIPanel panel;
+    public UIButton gameStartButtonColl;
+    public UISprite touch;
+    public GameObject movie;
+
+    public float timeCount;
+
+	// Use this for initialization
+	void Awake() {
+
+        panel.gameObject.SetActive(true);
+        movie.gameObject.SetActive(false);
+        touch.gameObject.SetActive(false);
+        gameStartButtonColl.gameObject.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timeCount > 0)
+        {
+            timeCount -= Time.deltaTime;
+            if (timeCount <= 0)
+            {
+                movie.gameObject.SetActive(true);
+                gameStartButtonColl.gameObject.SetActive(true);
+                touch.gameObject.SetActive(true);
+                panel.gameObject.SetActive(false);
+            }
+        }
+	
+	}
+}
